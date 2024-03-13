5a326ef7bd53bc6e3a6ee0ecb5d12c7a386467bb
Ander Mancisidor <andermanci6@gmail.com>
2024-03-13 16:08:25 +0100

    añadir tailwind colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,26 @@
+import defaultTheme from "tailwindcss/defaultTheme"
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				atomic: ["Atomic", "cursive"],
+			},
+			colors: {
+				primary: "var(--color-primary)",
+				secondary: "var(--color-secondary)",
+				accent: "var(--color-accent)",
+				twitch: "var(--color-twitch)",
+				ice: "var(--color-twitch-ice)",
+			},
+			screens: {
+				xs: "360px",
+				...defaultTheme.screens,
+				"3xl": "1650px",
+			},
+		},
 	},
 	plugins: [],
 }
