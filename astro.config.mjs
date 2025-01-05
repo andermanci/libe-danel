936fe3a269cf06a48d9c3564733d6796b3cea37d
Ander Mancisidor Pikabea <andermancisidorpikabea@MacBook-Pro-de-Ander-2.local>
2025-01-05 17:00:04 +0100

    añadir google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
+import partytown from '@astrojs/partytown';
 
 import preact from "@astrojs/preact";
 
